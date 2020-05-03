7a265476e602ec958887e80079c98a8cc78fb16b
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-05-03 08:38:07 +0300

    Add a command line argument to specify the batch size

--- main.py
+++ main.py
@@ -53,6 +53,8 @@ def build_argparser():
     parser.add_argument("-cy", "--concurrency", type=int, default=4,
                        help="Specifies the number of asynchronous "
                             "infer requests to perform at the same time")
+    parser.add_argument("-b", "--batch", type=int, default=1,
+                       help="Defines the frame batch size")                            
     parser.add_argument("-d", "--device", type=str, default="CPU",
                         help="Specify the target device to infer on: "
                              "CPU, GPU, FPGA or MYRIAD is acceptable. Sample "
