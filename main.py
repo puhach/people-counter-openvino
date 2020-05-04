a46bf2a1547b2034f4706b376d42d527538e0581
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-05-04 19:16:11 +0300

    Fix missing probability threshold command line argument

--- main.py
+++ main.py
@@ -46,25 +46,31 @@ def build_argparser():
                         help="Path to an xml file with a trained model.")
     parser.add_argument("-i", "--input", required=True, type=str,
                         help="Path to image or video file")
-    parser.add_argument("-l", "--cpu_extension", required=False, type=str,
-                        default=None,
-                        help="MKLDNN (CPU)-targeted custom layers."
-                             "Absolute path to a shared library with the"
-                             "kernels impl.")
 
-    parser.add_argument("-cy", "--concurrency", type=int, default=4,
-                       help="Specifies the number of asynchronous "
-                            "infer requests to perform at the same time")
+    parser.add_argument("-pt", "--prob_threshold", type=float, default=0.5,
+                        help="Probability threshold for detections filtering"
+                        "(0.3 by default)")
 
     parser.add_argument("-b", "--batch", type=int, default=1,
                        help="Defines the frame batch size")
 
+    parser.add_argument("-cy", "--concurrency", type=int, default=4,
+                       help="Specifies the number of asynchronous "
+                            "infer requests to perform at the same time")
 
     parser.add_argument("-d", "--device", type=str, default="CPU",
                         help="Specify the target device to infer on: "
                              "CPU, GPU, FPGA or MYRIAD is acceptable. Sample "
                              "will look for a suitable plugin for device "
                              "specified (CPU by default)")
+
+    parser.add_argument("-l", "--cpu_extension", required=False, type=str,
+                        default=None,
+                        help="MKLDNN (CPU)-targeted custom layers."
+                             "Absolute path to a shared library with the"
+                             "kernels impl.")
+
+
     return parser
 
 
